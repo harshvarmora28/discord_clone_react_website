6a149a14680dc27cd56f29270f50b309a6e1c1ff
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-10-19 12:17:53 +0530

    done with sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,14 @@ import React from 'react'
 import "./Sidebar.css";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import CallIcon from '@mui/icons-material/Call';
+import { Avatar } from '@mui/material';
+import MicIcon from '@mui/icons-material/Mic';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HeadsetIcon from '@mui/icons-material/Headset';
 
 
 const Sidebar = () => {
@@ -18,6 +26,34 @@ const Sidebar = () => {
                     <h4>Text Channels</h4>
                     </div>
                     <AddIcon className="sidebar__addChannel"/>
+                </div>
+                <div className="sidebar__channelsList">
+                <SidebarChannel/>
+                </div>
+            </div>
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large"/>
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon/>
+                    <CallIcon/>
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar src="https://avatars.githubusercontent.com/u/82329898?s=400&u=3b439f5eb95b40a7dae6232bf86681670f9ecb51&v=4"/>
+                <div className="sidebar__profileInfo">
+                    <h3>Harsh Varmora</h3>
+                    <p>#myid</p>
+                </div>
+                <div className="sidebar__profileIcons">
+                <MicIcon/>
+                <SettingsIcon/>
+                <HeadsetIcon/>
                 </div>
             </div>
         </div>
